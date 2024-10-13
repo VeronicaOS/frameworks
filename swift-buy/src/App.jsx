cd6aac7aa4b548e271985c2714a63701f0751ad3
VeronicaOS <verols51029@stud.noroff.no>
2024-10-13 23:00:30 +0200

    fixing some bugs

--- swift-buy/src/App.jsx
+++ swift-buy/src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/home";
@@ -7,11 +8,13 @@ import Layout from "./components/layout/layout";
 import { CartProvider } from "./context/cartContext";
 import CheckoutPage from "./pages/checkout/checkout";
 import CheckoutSuccessPage from "./pages/checkout/checkoutSuccess";
+import ScrollToTop from "./components/scroll/scroll"; // Import ScrollToTop
 
 const App = () => {
     return (
         <CartProvider>
             <Router>
+                <ScrollToTop /> {/* Add this here */}
                 <Layout>
                     <Routes>
                         <Route path="/" element={<HomePage />} />
